7a41981b91d6036a714004bbbfbbf641cc5635bf
André Glatz <andreglatz@outlook.com>
2020-10-16 20:20:48 -0300

    add struct CIDADE

--- main.c
+++ main.c
@@ -20,25 +20,7 @@ void imprimirGrafo(GRAFO *grafo) {
   }
 }
 
-CIDADE *criarCidades(int qtdeVertices) {
 
-  CIDADE *cidades = (CIDADE *) malloc(qtdeVertices * sizeof(CIDADE));
-
-  for (int i = 0; i < qtdeVertices; ++i) {
-
-    char cidade[50] = "CIDADE ";
-    cidade[7] = i + 65;
-    strcpy(cidades[i].nome, cidade);
-
-    double x = i * 0.5 + 2;
-    double y = i * 0.7 + 2;
-
-    cidades[i].coords.x = x <= 0 ? x + 0.01 : x;
-    cidades[i].coords.y = y <= 0 ? y + 0.01 : y;
-  }
-
-  return cidades;
-}
 
 int main(void) {
   int quantidadeVertices = 7;
